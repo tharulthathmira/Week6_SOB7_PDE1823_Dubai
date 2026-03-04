d3fc35e0fc0b12200074e203a71185d8b0d08339
tharulthathmira-personal <tharulthathmirattr@gmail.com>
2026-03-04 18:30:14 +0400

    Debugged the broken_pirate python file

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,6 @@
 greeting = input("Hello, possible pirate! What's the password?")
-if greeting in "Arrr!": # NG replaced ) by ]
+if greeting in ("Arrr!"): # NG replaced ) by ]
 	print("Go away, pirate.")
 else:
 	print("Greetings, hater of pirates!")
+	
