71fde379d101fa2c527bb45f6cd45e1a58304f1c
tharulthathmira-personal <tharulthathmirattr@gmail.com>
2026-03-05 12:33:34 +0400

    Debugged the tictactoe_broken python file

--- tictactoe_broken.py
+++ tictactoe_broken.py
@@ -98,12 +98,12 @@ if __name__ == '__main__':
     while winner == 0 and moves_exist(game):
         print("Currently player: " + str(player))
         available = False
-        while not available
+        while not available:
             row = convert_input_to_coordinate(int(input("Which row? (start with 1) ")))
             column = convert_input_to_coordinate(int(input("Which column? (start with 1) ")))
-            available = check_space_empty(game, row)
+            available = check_space_empty(game, row, column)
         game = add_piece(game, player, row, column)
         display_game(game)
         player = switch_player(player)
-#        winner = check_winner(game)
+        winner = check_winner(game)
     display_winner(winner)
